043b4b5138a735ebfe4b07a1ec6dc96344362a8d
Faizan Younas Tanooli <faizan@optyyl.com>
2024-09-24 17:50:36 +0500

    Updates to login form

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -3,28 +3,28 @@ import { FormGroup, Validators,FormsModule, ReactiveFormsModule, FormControl, No
 import { NzFormModule } from 'ng-zorro-antd/form';
 import { NzInputModule } from 'ng-zorro-antd/input';
 import { NzButtonModule } from 'ng-zorro-antd/button';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NzOptionComponent, NzSelectComponent } from 'ng-zorro-antd/select';
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [NzFormModule,NzInputModule,NzButtonModule,BrowserAnimationsModule,FormsModule,ReactiveFormsModule],
+  imports: [NzFormModule,NzInputModule,NzButtonModule,FormsModule,ReactiveFormsModule,NzSelectComponent,NzOptionComponent],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss'
 })
 export class LoginComponent {
   
-
+  loginForm: FormGroup<{
+    username: FormControl<string>;
+    password: FormControl<string>;
+  }>;
   constructor(private fb: NonNullableFormBuilder) {
 
     this.loginForm = this.fb.group({
-      email: ['', [Validators.email, Validators.required]],
+      username: ['', [Validators.email, Validators.required]],
       password: ['', [Validators.required]],
     });
   }
-  loginForm: FormGroup<{
-    email: FormControl<string>;
-    password: FormControl<string>;
-  }>;
+    
 
   ngOnInit(): void {
     
